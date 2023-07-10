7fec996f05389424c90dcf9ff700368badbe6aac
Mehran <hosseinzadehmehran4@gamil.com>
2023-07-10 08:31:38 +0330

    update(Model): add roles relation to permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+
+    protected $fillable=['title','description'];
+
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
